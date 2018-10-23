45f81e7bf2b73e6d14299f71b41bc09e9451f05e
Mariam Reintop <mreintop@tlu.ee>
2018-10-23 13:49:13 +0300

    esimene

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,27 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+    test('should show rentals as the home page', async function(assert){
+        
+    });
+    test('should link to the information about the company', async function(assert){
+        
+    });
+    test('should link to contact information', async function(assert){
+        
+    });
+    test('should list avaliable rentals', async function(assert){
+        
+    });
+    test('should filter the avaliable rentals by city', async function(assert){
+        
+    });
+    test('should show information about the rental', async function(assert){
+        
+    });
+    
+});
